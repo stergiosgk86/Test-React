bdcc20b9c67e637ee4e392acef60ac0c99e47d0d
stergiosgk <52173282+stergiosgk86@users.noreply.github.com>
2021-03-24 21:18:32 +0200

    Create react project and navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "../App.css";
+import worldwide from "../images/worldwide.png";
+import logo from "../images/logo-black.png";
+import search from "../images/magnifying-glass.png";
+import user from "../images/user2.png";
+import cart from "../images/shopping-carrt.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="text-white top-header d-flex align-items-center justify-content-center">
+        <div className="col-1"></div>
+        <div className="d-flex col">
+          <div className="language-icon">
+            <img src={worldwide} style={{ width: "22px" }} alt="" />
+          </div>
+          <div className="pl-3">Ελληνικά</div>
+        </div>
+      </div>
+      <div className="top-header-line py-0"></div>
+
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-light"
+        style={{ minHeight: "100px" }}
+      >
+        <div className="col-1"></div>
+        <a className="navbar-brand" href="#">
+          <img className="" src={logo} alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-center m-auto"
+          id="navbarSupportedContent"
+        >
+          <ul
+            className="navbar-nav text-uppercase font-weight-bold"
+            style={{ fontSize: "14px" }}
+          >
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                δωρα
+              </a>
+            </li>
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                εκτυπωσεις ενδυματων
+              </a>
+            </li>
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                αυτοκολλητα
+              </a>
+            </li>
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                καμβαδες
+              </a>
+            </li>
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                κεντηματα ενδυματων
+              </a>
+            </li>
+            <li className="nav-item pr-4">
+              <a className="nav-link text-dark" href="#">
+                offset εκτυπωσεις
+              </a>
+            </li>
+            <div className="buttons d-flex ">
+              <li className="btn nav-item pr-4">
+                <img src={search} style={{ width: "22px" }} alt="" />
+              </li>
+              <li className="btn nav-item pr-4">
+                <img src={user} style={{ width: "22px" }} alt="" />
+              </li>
+              <li className="btn nav-item pr-4">
+                <img src={cart} style={{ width: "22px" }} alt="" />
+              </li>
+            </div>
+          </ul>
+        </div>
+        <div className="col-md-1"></div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
